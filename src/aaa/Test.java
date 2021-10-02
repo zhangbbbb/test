ab2cfbe494b8aecf515f99970fa79f1d09997051
zhangbbbb <zhangya40419@qq.com>
2021-10-02 19:35:30 +0800

    x

--- src/aaa/Test.java
+++ src/aaa/Test.java
@@ -5,17 +5,17 @@ import java.util.Queue;
 
 public class Test {
 
-	public int[][] matrix(int[][] matrix){
-		int m = matrix.length;
-		int n = matrix[0].length;
-		
-		Queue<int[]> queue = new LinkedList<>();
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if(matrix[i][j]==0) {
-					queue.offer(e);
-				}
-			}
-		}
-	}
+//	public int[][] matrix(int[][] matrix){
+//		int m = matrix.length;
+//		int n = matrix[0].length;
+//		
+//		Queue<int[]> queue = new LinkedList<>();
+//		for (int i = 0; i < m; i++) {
+//			for (int j = 0; j < n; j++) {
+//				if(matrix[i][j]==0) {
+//					queue.offer(e);
+//				}
+//			}
+//		}
+//	}
 }
