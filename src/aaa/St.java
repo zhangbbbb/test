f7d55f192a377bc4792f1a71afa50a68fa851358
zhangbbbb <zhangya40419@qq.com>
2021-10-12 08:12:37 +0800

    0

---
+++ src/aaa/St.java
@@ -0,0 +1,40 @@
+package aaa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class St {
+//	public String minWindow(String s, String t) {
+//		if(s == null || s.length() == 0 || t==null||t.length()==0) {	
+//			return "";
+//		}
+//		int[] need = new int[128];
+//		for (int i = 0; i < t.length(); i++) {
+//			need[t.charAt(i)]++;
+//		}
+//		int count=0;
+//		for(char i='a';i<='z';i++) {
+//			if(need[i]>0) {
+//				count++;
+//			}
+//		}
+//		for(char i='A';i<='Z';i++) {
+//			if(need[i]>0) {
+//				count++;
+//			}
+//		}
+//		int[] window = new int[128];
+//			for (int i = 0; i < s.length(); i++) {
+//				window[s.charAt(i)]++;
+//				if(window[s.charAt(i)]==need[s.charAt(i)]) {
+//					count--;
+//					if (count==0) {
+//						break;
+//					}
+//				}
+//				
+//			}
+//			
+//		
+//    }
+}
